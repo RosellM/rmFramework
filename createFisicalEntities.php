41db8155daf44326aa96ac32ae986a13d7fca404
RosellM <charlotte0194@gmail.com>
2015-06-11 23:21:54 -0500

    v1.0.2
    error al crear entidades de dejo como el inicio

--- createFisicalEntities.php
+++ createFisicalEntities.php
@@ -59,10 +59,11 @@ CREAR ARCHIVO POR CADA CLASE
 		{
 			echo $Attttr."\n";
 			array_push ( $this->ClassParam , $Attttr );
-			$file = fopen("rmEntities/".$this->ClassName.".class.php","a+");
+			$file = fopen("rmEntities/".$this->ClassName.".php","a+");
 			fputs($file,"\npublic $".$Attttr.";");
-			fputs($file,"\npublic function set".$Attttr."($".$Attttr."){".$this->auxC."".$Attttr." = $".$Attttr." }\n");
-			fputs($file,"\npublic function get".$Attttr."(){return ".$this->auxC."".$Attttr." }\n\n");
+
+			fputs($file,"\npublic function set". strtoupper( $Attttr )."($".$Attttr."){".$this->auxC."".$Attttr." = $".$Attttr." }\n");
+			fputs($file,"\npublic function get". strtoupper( $Attttr )."(){return ".$this->auxC."".$Attttr." }\n\n");
 
  			fclose($file);
 		}
